06ef496ed4d6051ed6c5ecb604c877881af00a72
Jit Sengupta <amdroid007@gmail.com>
2022-10-22 12:31:54 -0400

    new files
    initial commit

---
+++ LightController.py
@@ -0,0 +1,10 @@
+"""
+This is the lightcontroller demo class
+"""
+class LightController:
+
+    def __init__(self) -> None:
+        print("LightController constructor")
+
+    def addLight(self, newlight):
+        print("Add a new light to lightcontroller")
